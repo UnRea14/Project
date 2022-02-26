6cccf66d8f70ea454c6c6cc47912d35773aef7e2
liams <liamshani14@gmail.com>
2022-02-26 16:16:43 +0200

    started user info saving in the database

--- Server.py
+++ Server.py
@@ -1,9 +1,17 @@
 import socket
 import ssl
+import mysql.connector
 import select
 
 
 def main():
+    db = mysql.connector.connect(
+        host="",  # change to real host
+        user="",  # change to real user
+        password="",  # change to real password
+        database=""  # change to real database
+    )
+    db_cursor = db.cursor()
     messages_to_send = []
     client_sockets = []
     server_sock = socket.socket()
@@ -13,36 +21,36 @@ def main():
         rlist, wlist, elist = select.select([server_sock] + client_sockets, client_sockets, [], 0.01)
         for sock in rlist:
             if sock is server_sock:
-                connection, client_addr = sock.accept()
+                connection, client_address = sock.accept()
                 client_sockets.append(connection)
-                message = "005Hello"
-                messages_to_send.append((connection, message))
-
             else:
                 try:
                     length = sock.recv(3).decode()
                     if length == '':  # sock disconnected
                         client_sockets.remove(sock)
                         sock.close()
-
                     else:
                         try:
                             data = sock.recv(int(length)).decode()
                             print(data)
-                            if data == "upload file":
-                                message = "002ok"
-                                messages_to_send.append((sock, message))
-
+                            if "|" in data:  # insert new user into users table
+                                split_data = data.split("|")
+                                user_name = split_data[0]
+                                user_email = split_data[1]  # later need to encrypt the password
+                                user_password = split_data[2]
+                                sql_command = "INSERT INTO users (name, email, password) VALUES (%s, %s, %s)"
+                                values = (user_name, user_email, user_password)
+                                db_cursor.execute(sql_command, values)
+                                db.commit()
+                                print(f"Inserted user {user_name}")
                         except ValueError:
                             print("Value Error")
                             client_sockets.remove(sock)
                             sock.close()
-
                 except ConnectionResetError:
                     print("Connection Reset Error")
                     client_sockets.remove(sock)
                     sock.close()
-
         for message in messages_to_send:
             current_socket, data = message
             for sock in client_sockets:
--- test_client.py
+++ test_client.py
@@ -4,9 +4,14 @@ import ssl
 
 
 def main():
+    email = "liam14@gmail.com"
+    password = "liam1402"
+    name = "liam"
+    first_time = True
     client_sock = socket.socket()
     try:
         client_sock.connect(("127.0.0.1", 8820))
+        print("Connected")
         while True:
             rlist, wlist, elist = select.select([client_sock], [client_sock], [])
             for sock in rlist:
@@ -15,16 +20,17 @@ def main():
                     try:
                         data = sock.recv(int(length)).decode()
                         print(data)
-                        if data == "Hello":
-                            sock.send("011upload file".encode())
+                        if first_time:
+                            message = name + "|" + email + "|" + password
+                            length = str(len(message))
+                            sock.send((length.zfill(3) + message).encode())
+                            first_time = False
                     except ValueError:
                         print("Value Error")
                         quit()
-
                 except ConnectionResetError:
                     print("Connection Reset Error")
                     quit()
-
     except ConnectionError:
         print("Connection Error - Server offline")
         quit()
